e8d308532adcf29cf7cdecf38a66ff4f4b95b1f7
agustin <agustinaleks@gmail.com>
2021-02-19 01:29:22 -0300

    first commitMERN

---
+++ README.md
@@ -0,0 +1,2 @@
+index.html es la página principal del proyecto, que consume el archivo JS app.js aplicando
+los estilos establecidos en el archivo styles.css
---
+++ app.js
@@ -0,0 +1,10 @@
+document.write ("<h1>Hello Pilar Tecno 2021</h1>")
+
+
+var fechaHora = new Date();
+
+var fechaHora = new Date();
+document.write (fechaHora);
+
+
+
